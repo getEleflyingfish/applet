c422ada17edd8b88fa98c53b3e845e4756e8bf4c
getEleflyingfish <935963409@qq.com>
2019-08-10 18:45:26 +0800

    ...+3

--- wex/pages/supply/supply.js
+++ wex/pages/supply/supply.js
@@ -1,15 +1,28 @@
 Page({
     data: {
         collSrc: '../images/tel_03.png',
-        left:'-100%',
-
+        hidden: true,
+        dataArr: [
+            {
+                title: "条目",
+                list: [{
+                    listId: 1,
+                    listId: 2
+                }]
+            },
+            {
+                title: "分类",
+                list: [{
+                    listClass: "蔬菜",
+                    listClass: "蔬菜"
+                }]
+            }
+        ]
     },
-    tapDownload: function () {
-        console.log("111111")
-         this.setDate({ 
-            left:'0%'
-         }) 
-         this.left='0%'
-
+    tapDownload() {
+        const hidden = !this.data.hidden;
+        this.setData({
+            hidden
+        })
     }
 })
